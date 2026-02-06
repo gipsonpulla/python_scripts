7dabe6b8d1eff8fe1f6ca5fe6d08dfe1ba54e8f4
Gipson Pulla <pullagipson@gmail.com>
2026-02-06 17:47:58 +0530

    adding the first python fun

---
+++ function1.py
@@ -0,0 +1,27 @@
+#user defined function
+def add(greet):
+    print ("Hello" + greet)
+
+""" if __name__ == "__main__":
+    add("world")
+ """
+
+add("world")
+
+'''
+def add2():
+    a = int(input("Enter the num a"))
+    b = int(input("Enter the num b"))
+    print (f"Answer is {a + b}")
+
+add2()
+''
+
+name = "Gipson"
+age = 38
+gender = 'male'
+
+print (f"My name is {name}")
+print (f"My age is {age}")
+print (f"My gender is {gender}")
+'''
