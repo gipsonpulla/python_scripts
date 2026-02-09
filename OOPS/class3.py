27d153c1df70521fd67901d64f21ba7a357056d5
Gipson Pulla <pullagipson@gmail.com>
2026-02-09 13:14:54 +0530

    adding args to methods

---
+++ OOPS/class3.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self) -> None:
+        self.name = input("Enter name = ")
+        self.age = input("Enter age =")
+    
+    def display_info(self):
+        print (f"Name = {self.name}")
+        print (f"Age = {self.age}")
+
+    def change_name(self, new_name: str):
+        self.name = new_name
+
+s1 = Student()
+s1.display_info()
+s1.change_name("xya")
+s1.display_info()
+
+
