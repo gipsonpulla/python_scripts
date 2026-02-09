c2f8aa40df7489efebe0e120a98eeab51b6e5329
Gipson Pulla <pullagipson@gmail.com>
2026-02-09 09:19:34 +0530

    adding the banking script

--- OOPS/banking.py
+++ OOPS/banking.py
@@ -6,7 +6,12 @@ class Bank:
         self.name = input("Enter the name = ")
         self.balance = 0
         self.phone_number = int(input("Enter phone number="))
-    
+
+    def show_info(self):
+        print(f"Account number = {self.account}")
+        print(f"Full name = {self.name}")
+        print (f"Balance = {self.balance}")               
+
     def show_balance(self):
         print (f"current balance = {self.balance}")
     
@@ -35,15 +40,29 @@ banks = []
 while True:
     print("1. create an account")
     print("2. show all bank details")
-    print("3. Exit")
+    print("3. Deposit amount")
+    print("4. Exit")
     choice = int(input("Enter the choice = "))
     if choice == 1:
         obj = Bank()
         banks.append(obj)
         print (banks)
     elif choice == 2:
-        pass
+        if len(banks) == 0:
+            print ("No accounts created")
+        else:
+            for account in banks:
+                account.show_info()
     elif choice == 3:
+        if len(banks) == 0:
+            print("No banks found")
+        else:
+            account = int(input("Enter the accont nu to deposit"))
+            for obj in banks:
+                if obj.account == account:
+                    obj.deposit()
+                break
+    elif choice == 4:
         break
     else:
         print("Invalid choice")
